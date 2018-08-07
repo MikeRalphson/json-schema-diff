932c23d62bef72346394d8a1665386e2807db9bf
Sebastian Tello <stello@atlassian.com>
2018-08-07 11:37:26 +1000

    chore: run pipelines tests for supported node versions in parallel

--- bin/build.sh
+++ bin/build.sh
@@ -1,21 +1,3 @@
-echo ==== Load nvm ====
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
-
-echo ==== Test using node 6.x ====
-nvm install 6.14.1
-rm -rf node_modules
+echo "Node Version: $(node --version)"
 npm install
-npm test
-
-echo ==== Test using node 8.x ====
-nvm install 8.11.1
-rm -rf node_modules
-npm install --no-shrinkwrap
-npm test
-
-echo ==== Test using node 9.x ====
-nvm install 9.11.1
-rm -rf node_modules
-npm install --no-shrinkwrap
 npm test
